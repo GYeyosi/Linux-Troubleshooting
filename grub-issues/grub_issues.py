503faa826ebc0ab77e4bc62c0084accc16d865cd
fl4shi3r <fl4shi3r@gmail.com>
2018-06-18 12:55:08 +0530

    Placed in folder

---
+++ grub-issues/grub_issues.py
@@ -0,0 +1,14 @@
+print("""After restarting your system if you get following messege:
+Entering rescue mode...
+grub rescue>
+Then enter the following commands:
+1.set (it provide you values of these: cmdpath, prefix, root)
+2.set boot=(put_here_the_value_of_root)
+3.set prefix=(put_here_the_value_of_prefix)
+4.insmod normal""")
+ 
+
+ print("""if, after instlation linux you directly boot into your windows with out loading your boot loader.
+ Then before restarting your linux after installation:
+ 1.run the repair.sh file from the terminal (bash repair.sh)
+ 2.then follows the instructions""")
